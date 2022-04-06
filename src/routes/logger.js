2cf62945a68134472a364a7f5bf333e1e306d789
saurabh2160 <saurabhmanohar90@gmail.com>
2022-04-06 17:31:42 +0530

    new change

---
+++ src/routes/logger.js
@@ -0,0 +1,6 @@
+let end = 'https://www.google.com'
+let logg= function(){
+    console.log("I am inside log fucntion")
+}
+module.exports.url= end;//public maker
+module.exports.logging=logg;//public maker
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const logger=require('./logger')
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
+    console.log('the endpoint value is',logger.url)
+    logger.logging()
     res.send('My first ever api!')
+    
+    
 });
 
 module.exports = router;
