9204eadb563313f16ee7383812c41bd621df7fdb
saurabh2160 <saurabhmanohar90@gmail.com>
2022-04-07 19:52:11 +0530

    y

--- src/lodash/union.js
+++ src/lodash/union.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const _=require('lodash')
+const lodash=require('lodash')
 
 const router = express.Router();
 
 router.get('/hello-me', function (req, res) {
-    console.log(_.union([1,2,1],[3,5,3],[4,1,5],[5,1,5],[6,5,6]))
+    console.log(lodash.union([1,2,1],[3,5,3],[4,1,5],[5,1,5],[6,5,6]))
     res.send('My first ever api!')
 });
 module.exports = router;
